1bf8f8e2d08944ec7946bca6d28ae979926716b8
LeticiaChristie <bertinneleticia@gmail.com>
2023-01-20 17:26:33 -0300

    Erros node

---
+++ erros-node/throw.js
@@ -0,0 +1,5 @@
+const n1 = "10"
+
+if (!Number.isInteger(n1)){
+    throw new Error("The value of n1 doesn't interger.")
+}
---
+++ erros-node/try-catch.js
@@ -0,0 +1,8 @@
+const n2 = 12
+
+try {
+ n2 = 14
+}
+catch (err){
+    console.log(`Error: ${err}`);
+}
